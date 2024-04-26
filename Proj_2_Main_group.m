75f45113c3d35010ee298abe444cb6cff9f88b20
Alden Roswell <153668548+DogePool531@users.noreply.github.com>
2024-04-25 19:02:55 -0600

    Add files via upload

---
+++ Proj_2_Main_group.m
@@ -0,0 +1,124 @@
+%{
+Author: Alden Roswell
+Assignment: Project 2 indidvidual submission
+Creation Date: 4/15/24
+Inputs: getConst.m OdeFun.m Thrust.m
+Outputs: Flight path of bottle rocket
+Purpose: to predict different times of a bottle rocket with distinct
+phases of flight
+%}
+
+%% Notes
+% Any variable ending in "_i" is an initial condition
+
+%% Common practices
+clear;
+clc;
+close all;
+format long;
+%% scale values
+
+Design_space_dimension = 3;
+
+delta_theta = deg2rad(1); % radians
+delta_p_i = 1 * 6894.76; % pascals
+delta_w_i = 0.0001; % m^3
+
+gradient_scalar = [1/10000, 2000000, 1/10000];
+
+delta_vals = [delta_theta, delta_p_i, delta_w_i];
+
+const = getConst();
+
+
+for i = 1:1000
+
+    delta_matrix = zeros(Design_space_dimension);
+    const_vector = [const.theta_i;const.p_r_i;const.Vol_w_i];
+
+    for k = 1:3
+        delta_matrix(k,k) = delta_vals(k);
+    end
+
+    for j = 1:Design_space_dimension + 1
+        %% Change Constant Function
+
+        if j > 1
+            const_vector = const_vector + delta_matrix(:,j-1);
+        end
+
+        test_const = const;
+        test_const.theta_i = const_vector(1);
+        test_const.p_r_i = const_vector(2);
+        test_const.Vol_w_i = const_vector(3);
+
+        %% Determing initial conditions based on constants
+        Vol_air_i = test_const.Vol_bottle - test_const.Vol_w_i; %
+        m_air_i = (Vol_air_i * test_const.p_r_i)/(test_const.R_air * test_const.T_i); % mass of air at launch.
+        m_r_i = test_const.m_bottle + test_const.row_w * test_const.Vol_w_i + m_air_i; % mass of rocket at launch;
+        timespan = [0,10];
+        %% creating initial state vector From initial Conditions.
+        state_i = [test_const.x_i; 0; test_const.z_i; 0; m_r_i; Vol_air_i; m_air_i];
+        %% Running ODE45
+        state = [];
+        [t,state] = ode45(@(t,state) OdeFun(t,state,test_const,m_air_i), timespan, state_i);
+        distance_test(i,j) = max(state(:,1));
+
+    end
+
+    for l = 1:Design_space_dimension
+ 
+        delta_dist(i,l) = ((distance_test(i,l+1) - distance_test(i,1)) / delta_vals(l));
+    end
+
+    update_vector = delta_dist(i,:) .* gradient_scalar;
+    
+
+    const.theta_i = max([min([const.theta_i + update_vector(1), deg2rad(80)]), 0]);
+    const.p_r_i = max([min([const.p_r_i + update_vector(2), 85 * 6894.76]), 0 ]);
+    const.Vol_w_i = max([min([const.Vol_w_i + .000001 * update_vector(3), const.Vol_bottle]), 0]);
+
+
+
+
+end
+
+const.p_r_i / 6894.76
+
+
+%% Finding the Force Values with Thrust Function that mimics OdeFun
+% Thrust() is a modified OdeFun that returns thrust and phase rather than
+% state changes.
+F_Thrust = zeros(length(t),1);
+for i = 1:length(t)
+    [F_thrust(i),phase(i)] = Thrust(state(i,:),(const),m_air_i);
+end
+%% Finding answers
+answers.MaxThrust = max(F_thrust)
+answers.MaxAltitude = max(state(:,3));
+answers.MaxDistance = max(state(:,1))
+answers;
+
+%% Plot Thrust vs Time
+figure()
+title('Thrust Over Time')
+hold on;
+plot(t,F_thrust(:), 'LineWidth',1)
+%xline(t(length(phase(phase == 1))), 'color', 'green', 'LineWidth',1);
+%xline(t(length(phase(phase <= 2))), 'color', 'yellow', 'LineWidth',1);
+grid("on")
+xlabel('Time (s)')
+ylabel('Thrust (N)')
+
+
+%% Plot Trajectory
+figure()
+hold on;
+title('Rocket Trajectory')
+plot(state(:,1),state(:,3))
+
+xlabel('Distance (m)')
+ylabel('Altitude (m)')
+
+grid on;
+
